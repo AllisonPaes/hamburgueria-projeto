28e17e8a2c0e3b23bb7b170b27e6be93c4c800f9
AllisonPaes <alisonpablo03@gmail.com>
2024-05-10 16:53:53 -0300

    Script.js: updateCartModal

--- script.js
+++ script.js
@@ -70,25 +70,36 @@ function updateCartModal(){
 
     cart.forEach(item => {
         const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col")
         
         cartItemElement.innerHTML = `
-            <div>
+            <div class="flex items-center justify-between">
                 <div>
-                    <p>${item.name}</p>
-                    <p>${item.quantity}</p>
-                    <p>R$ ${item.price}</p>
+                    <p class="font-medium">${item.name}</p>
+                    <p>Qtd: ${item.quantity}</p>
+                    <p class="font-medium mt-2">R$ ${item.price.toFixed(2)}</p>
                 </div>
-            <div>
+
+            
                 <button>
                     Remover
                 </button>
-            </div>
+            
             </div>
         `
+
+        total += item.price * item.quantity;
 
         cartItemsContainer.appendChild(cartItemElement)
 
 
     })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerText = cart.length;
 
 }
